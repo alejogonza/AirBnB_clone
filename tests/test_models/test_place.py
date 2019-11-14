bb7271f8837ea774e70e7a9ce110836e8eda8b62
dbravo0 <stevenbravobel@gmail.com>
2019-11-13 20:14:57 -0500

    [fix] place test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""test for place"""
+"""clase para place"""
 import unittest
 import os
 from models.place import Place
@@ -8,48 +8,46 @@ import pep8
 
 
 class TestPlace(unittest.TestCase):
-    """this will test the place class"""
+    """clase para place class"""
 
     @classmethod
-    def setUpClass(cls):
-        """set up for test"""
+    def Config(cls):
         cls.place = Place()
-        cls.place.city_id = "1234-abcd"
-        cls.place.user_id = "4321-dcba"
-        cls.place.name = "Death Star"
-        cls.place.description = "UNLIMITED POWER!!!!!"
-        cls.place.number_rooms = 1000000
-        cls.place.number_bathrooms = 1
-        cls.place.max_guest = 607360
-        cls.place.price_by_night = 10
-        cls.place.latitude = 160.0
-        cls.place.longitude = 120.0
-        cls.place.amenity_ids = ["1324-lksdjkl"]
+        cls.place.city_id = "CaCa-666"
+        cls.place.user_id = "999-AcAc"
+        cls.place.name = "IBIZA"
+        cls.place.description = "melo el cuarto"
+        cls.place.number_rooms = 666
+        cls.place.number_bathrooms = 8
+        cls.place.max_guest = 2
+        cls.place.price_by_night = 6666666
+        cls.place.latitude = 90.0
+        cls.place.longitude = 100.0
+        cls.place.amenity_ids = ["CoCo-6969"]
 
     @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
+    def Cleardelete(cls):
         del cls.place
 
-    def tearDown(self):
-        """teardown"""
+    def ClearDelete(self):
+        """ClearDelete"""
         try:
             os.remove("file.json")
         except Exception:
             pass
 
-    def test_pep8_Place(self):
-        """Tests pep8 style"""
+    def Place_style(self):
+        """pep8"""
         style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/place.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        pep = style.check_files(['models/place.py'])
+        self.assertEqual(pep.total_errors, 0, "fix pep8")
 
-    def test_checking_for_docstring_Place(self):
-        """checking for docstrings"""
+    def Place_docstring(self):
+        """docstrings"""
         self.assertIsNotNone(Place.__doc__)
 
-    def test_attributes_Place(self):
-        """chekcing if amenity have attributes"""
+    def Place_attributes(self):
+        """attributes"""
         self.assertTrue('id' in self.place.__dict__)
         self.assertTrue('created_at' in self.place.__dict__)
         self.assertTrue('updated_at' in self.place.__dict__)
@@ -65,12 +63,12 @@ class TestPlace(unittest.TestCase):
         self.assertTrue('longitude' in self.place.__dict__)
         self.assertTrue('amenity_ids' in self.place.__dict__)
 
-    def test_is_subclass_Place(self):
-        """test if Place is subclass of Basemodel"""
+    def Place_subclass(self):
+        """Basemodel"""
         self.assertTrue(issubclass(self.place.__class__, BaseModel), True)
 
-    def test_attribute_types_Place(self):
-        """test attribute type for Place"""
+    def Place_attrib(self):
+        """test attrib"""
         self.assertEqual(type(self.place.city_id), str)
         self.assertEqual(type(self.place.user_id), str)
         self.assertEqual(type(self.place.name), str)
@@ -83,13 +81,13 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(type(self.place.longitude), float)
         self.assertEqual(type(self.place.amenity_ids), list)
 
-    def test_save_Place(self):
-        """test if the save works"""
+    def Place_save(self):
+        """save"""
         self.place.save()
         self.assertNotEqual(self.place.created_at, self.place.updated_at)
 
-    def test_to_dict_Place(self):
-        """test if dictionary works"""
+    def Place_dict(self):
+        """dict"""
         self.assertEqual('to_dict' in dir(self.place), True)
 
 
